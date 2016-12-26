6e03e845e75df410c5df1d206b2a595b2c1fd70d
LouisHsu <r05922162@ntu.edu.tw>
2016-12-27 00:49:05 +0800

    normal

--- hw1/hw1.m
+++ hw1/hw1.m
@@ -1,36 +1,5 @@
 function hw1()
-    X = [];
-    Y_head = [];
-    
-    [x, y_head] = generateRandomDate(0.2, 1.8, 0.2, 1.8, 1);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    [x, y_head] = generateRandomDate(2.2, 3.8, 0.2, 1.8,0);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    [x, y_head] = generateRandomDate(4.2, 5.8, 0.2, 1.8,1);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    [x, y_head] = generateRandomDate(0.2,1.8,2.2,3.8,1);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    [x, y_head] = generateRandomDate(2.2,3.8,2.2,3.8,1);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    [x, y_head] = generateRandomDate(4.2,5.8,2.2,3.8,0);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    [x, y_head] = generateRandomDate(0.2,1.8,4.2,5.8,0);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    [x, y_head] = generateRandomDate(2.2,3.8,4.2,5.8,1);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    [x, y_head] = generateRandomDate(4.2,5.8,4.2,5.8,1);
-    X = [X x];
-    Y_head = [Y_head y_head];
-    
-%     [X, Y_head] = parseData('hw1data.dat');
+    [X, Y_head] = parseData('hw1data.dat');
     layers = [2 4 3 1];
     global layerLength;
     layerLength = length(layers);
@@ -45,40 +14,17 @@ function [X, Y_head] = parseData(file_name)
     for i = find(Y_head < 0);
         Y_head(i) = 0;
     end
+
+    plot(X(1, find(Y_head == 0)), X(2, find(Y_head == 0)), 'x', X(1, find(Y_head == 1)), X(2, find(Y_head == 1)), 'o');
 end
-
-function [X, Y_head] = generateRandomDate(x0, x1, y0, y1, value)
-    x = rand(1, 100) * (x1-x0) + x0;
-    y = rand(1, 100) * (y1-y0) + y0;
-    X = [x; y];
-    Y_head = zeros(1, 100);
-    Y_head = Y_head + value;
-end
-
-
-% function initWeights(layers)
-%     global weightList;
-%     weightList = cell(1, length(layers)-1);
-%     for i = 2:length(layers)
-%         Wij = [];
-%         for j = 1: layers(i)
-%             Weights = (rand(1, layers(i-1) + 1)' - 0.5);
-%             Wij = [Wij Weights];
-%         end
-%         weightList{i-1} = Wij;
-%     end
-%     disp(weightList);
-% end
 
 function initWeights(layers)
     global weightList;
     weightList = cell(1, length(layers)-1);
-    Wij = [1 1 0 0; 0 0 1 1; -2 -4 -2 -4];
-    weightList{1} = Wij;
-    for i = 3:length(layers)
+    for i = 2:length(layers)
         Wij = [];
         for j = 1: layers(i)
-            Weights = (rand(1, layers(i-1) + 1)' - 0.5);
+            Weights = (rand(1, layers(i-1) + 1)'  )  ;
             Wij = [Wij Weights];
         end
         weightList{i-1} = Wij;
@@ -87,12 +33,9 @@ function initWeights(layers)
 end
 
 function training(X, Y_head)
-    
     global weightList;
-    cur = pwd;
-    disp(cur);
-    file_name = fopen('C:\Users\Selab\Documents\NN\NN-HW1\error.txt', 'w');
-    for j = 1 : 200
+    index = 1;
+    for j = 1 : 1000
         disp(j);
         sigma = 0;
         [~, c] = size(X);
@@ -105,24 +48,21 @@ function training(X, Y_head)
             deltaList = inToHide(netList, deltaList);
             updateWeights(netList, deltaList);
             sigma = sigma + error;
+            index = index + 1;
         end
         disp(sigma);
-        fprintf(file_name, '%f\n', sigma);
+        index = index + 1;
     end
-    fclose(file_name);
     
-    plot(X(1, find(Y_head == 0)), X(2, find(Y_head == 0)), 'x', X(1, find(Y_head == 1)), X(2, find(Y_head == 1)), 'o');
     for weights = weightList{1}
-        a = 0 : 1: 6;
-        %a = 0: 0.1: 1;
+        a = 0 : 0.1: 1;
         w1 = weights(1);
         w2 = weights(2);
         w3 = weights(3);
         b = ((-w1*a) - w3)/w2;
         hold on;
         plot(a, b, 'r');
-        ylim([0 6]);
-        %ylim([0 1]);
+        ylim([0 1]);
         hold off;
     end
 
@@ -131,14 +71,12 @@ end
 function updateWeights(netList, deltaList)
     global weightList;
     learning_rate = 0.5;
-    for i = length(weightList) : -1 : 2
+    for i = fliplr(1:length(weightList))
         Net_i = netList{i};
         Delta = deltaList{i};
         
         W = weightList{i};
-        Delta = Delta';
-        A = learning_rate .* [Net_i;1] * Delta;
-        W = W + A;
+        W = W + learning_rate .* [Net_i;1] * Delta';
         weightList{i} = W;
     end
 end
@@ -151,7 +89,7 @@ function [y, netList] = neuralOutput(Neti)
     for i = 1 : length(weightList)
         W = weightList{i};
         Z = W' * [Neti; 1];
-        Netj = 1 ./ (1 + exp(-Z)); 
+        Netj = sigmoid(Z); 
         netList{i+1} = Netj;
         Neti = Netj;
     end
@@ -175,21 +113,14 @@ end
 function deltaList = inToHide(netList, deltaList)
     global weightList;
     Delta = deltaList{length(deltaList)};
-    for i = length(weightList) - 1 : -1 : 2
+    for i = fliplr(1: length(weightList) - 1)
         Net_i = netList{i};
         Net_j = netList{i+1};
+        
         W = weightList{i+1};
         [w_r, ~] = size(W);
-        Delta = W(1:w_r-1, :) * Delta .* Net_j .* (1.0-Net_j);
+        Delta = W(1:w_r - 1, :) * Delta .* Net_j .* (1.0-Net_j);
 
-%         git  = [];
-%         for j = 1 : length(Net_j)
-%             net_j = Net_j(j);
-%             W_jk = getRowW(i+1,j);
-%             delta = (W_jk * Delta) * net_j * (1.0-net_j);
-%             delta_j = [delta_j delta];
-%         end
-%         Delta = delta_j';
         deltaList{i} = Delta;
     end
 end
@@ -198,14 +129,6 @@ function error = getError(y, y_head)
 	error = (y - y_head).^2;
 end
 
-function W = getColW(layer, index)
-	global weightList;
-	W = weightList{layer};
-	W = W(:, index);
-end
-
-function W = getRowW(layer, index)
-    global weightList;
-    W = weightList{layer};
-	W = W(index, :);
+function value = sigmoid(z)
+	value =  1 ./ (1 + exp(-z));
 end
