8a35f22b41d5db14a00f8ce58d68590ca522b489
LouisHsu <r05922162@ntu.edu.tw>
2017-01-11 13:35:58 +0800

    report first commit

--- hw2/DNN.py
+++ hw2/DNN.py
@@ -6,6 +6,7 @@ Created on Tue Dec 13 13:43:00 2016
 """
 import numpy as np
 import sys
+import random
 from FileHandler import FileHandler
 
 class DNN (object):
@@ -61,6 +62,9 @@ class DNN (object):
         X = self.X
         Y = self.Y
         
+        max_log_file = open("max.txt", 'w')
+        min_log_file = open("min.txt", 'w')
+        label_file = open("label.txt", 'w')
         for m in range(len(self.W)):
             Wm = self.W[m]
             Ym = None
@@ -77,21 +81,38 @@ class DNN (object):
                 q = -1
                 r = -1
                 s = -1
-                for i in range(X.shape[0]):
-                    for j in range(i+1, X.shape[0]):
-                        yi = Ym[i]
-                        yj = Ym[j]
-                        dist = np.linalg.norm(yi - yj)
-                        if Y[i] == Y[j]:
-                            if dist > maxArg:
-                                maxArg = dist
-                                p = i
-                                q = j
-                        else:
-                            if dist < minArg:
-                                minArg = dist
-                                r = i
-                                s = j
+                
+                if epoch <= -1:
+#                    print Y.shape[0]
+                    ran1 = random.randint(0, Y.shape[0]-1)
+                    ran2 = random.randint(0, Y.shape[0]-1)
+                    while(Y[ran1] != Y[ran2]):
+                        ran2 = random.randint(0, Y.shape[0] - 1)
+                    p = ran1
+                    q = ran2
+                    
+                    ran1 = random.randint(0, Y.shape[0]-1)
+                    ran2 = random.randint(0, Y.shape[0]-1)
+                    while(Y[ran1] == Y[ran2]):
+                        ran2 = random.randint(0, Y.shape[0] - 1)
+                    r = ran1
+                    s = ran2
+                else:
+                    for i in range(X.shape[0]):
+                        for j in range(i+1, X.shape[0]):
+                            yi = Ym[i]
+                            yj = Ym[j]
+                            dist = np.linalg.norm(yi - yj)
+                            if Y[i] == Y[j]:
+                                if dist > maxArg:
+                                    maxArg = dist
+                                    p = i
+                                    q = j
+                            else:
+                                if dist < minArg:
+                                    minArg = dist
+                                    r = i
+                                    s = j
                 
                 # Adjust Weight
                 eAtt = self._computeEAtt(Ym, Yn, p, q)
@@ -101,6 +122,8 @@ class DNN (object):
                 
                 if epoch % 100 == 0:
                     print "---------------Training %dth layer %d epoch---------------" %(m, epoch)
+                    max_log_file.write(str(maxArg) + "\n")
+                    min_log_file.write(str(minArg) + "\n")
                     print "MaxArg: " + str(maxArg)
                     print "MinArg: " + str(minArg)
                     
@@ -112,10 +135,14 @@ class DNN (object):
         Ym = Ym.T
         for i in range(Ym[0].shape[0]):
             if Ym[0][i] > 0.5:
-                Ym[0][i] = 1
-            else:
                 Ym[0][i] = 0
+            else:
+                Ym[0][i] = 1
+        for i in range(Ym[0].shape[0]):
+            label_file.write(str(Y[i]) + " " + str(Ym[0][i]) + "\n")
         print np.square(np.linalg.norm(Y - Ym[0]))
+        max_log_file.close()
+        min_log_file.close()
 
     
 if __name__ == '__main__':
